3b53c0f45d1b9573712069e30437c2bf6b85d10e
pharmgatsby <pharmgatsby981@gmail.com>
2023-03-03 09:53:19 -0800

    temi di owo e baba

---
+++ 131-heap_insert.c
@@ -0,0 +1,25 @@
+#include "binary_trees.h"
+
+/**
+ * height - measures the height of a tree
+ *
+ * @tree: tree root
+ * Return: height
+ */
+int height(const binary_tree_t *tree)
+{
+	int left = 0;
+	int right = 0;
+
+	if (tree == NULL)
+		return (-1);
+
+	left = height(tree->left);
+	right = height(tree->right);
+
+	if (left > right)
+		return (left + 1);
+
+	return (right + 1);
+}
+
---
+++ README.md
@@ -0,0 +1,1 @@
+will revist in no time
